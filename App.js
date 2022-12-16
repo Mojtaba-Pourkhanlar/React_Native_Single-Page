6326bc168396d811fab1ae12bb6e2697cea242e4
“[Mojtaba-Pourkhanlar]” <[mojtabapourkhanlar@gmail.com]>
2022-12-16 13:53:15 +0330

    fixed project

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, ImageBackground, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ImageBackground
+      style={styles.container}
+      source={require("./assets/wellcome.jpg")}>
+      <View style={styles.text}>
+        <Text style={styles.firstText}>Wellcome to my app</Text>
+      </View>
+
+      <View style={{ width: "90%", marginBottom: 10 }}>
+        <View style={styles.btnConttainer}>
+          <Button title="Login" color="#20295B" />
+        </View>
+        <View style={styles.btnConttainer}>
+          <Button title="Register" color="#08342E" />
+        </View>
+      </View>
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  text: {
+    alignItems: "center",
+  },
+  firstText: {
+    color: "#BCA900",
+    fontSize: 35,
+    marginTop: 20,
+  },
+  btnConttainer: {
+    padding: 15,
+    width: "100%",
+    marginVertical: 5,
   },
 });
